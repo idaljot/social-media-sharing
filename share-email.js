4b1bb7222e0716d6a08097bedc3124e1618f96e9
Daljot <daljot.singh@samsung.com>
2020-11-18 14:17:02 -0800

    DS: Added social media sharing JS files.

---
+++ share-email.js
@@ -0,0 +1,34 @@
+function emailFunction() {
+
+    var link = document.location.href
+    const data = JSON.stringify({
+        "destination": link,
+    });
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("readystatechange", function() {
+        if (this.readyState === this.DONE) {
+            var responseObject = JSON.parse(this.responseText);
+            var shortUrl = responseObject.shortUrl;
+
+            n = getSelection().anchorNode;
+            if (!n) {
+                t = document.title;
+            } else {
+                t = n.nodeType === 3 ? n.data : n.innerText;
+            }
+            t = 'Check this site:\n';
+            window.open(
+                `mailto:?body=${encodeURIComponent(t)}${encodeURIComponent(shortUrl)}`
+            );
+        }
+    });
+
+    xhr.open("POST", "https://api.rebrandly.com/v1/links");
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.setRequestHeader("apikey", "<your-api-key-here>");
+    xhr.send(data);
+
+
+}
